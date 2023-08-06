03ab297c1d7003cfd35cb5b990bf9992a4e35fcd
Vyaznikova Kristina <kristina-vyaz@yandex.ru>
2023-08-06 08:42:21 +0300

    Спринт 1 финал

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,35 @@ import profpage from "./src/layouts/profpage/profpage";
 import popup from "./src/layouts/popup/popup";
 import if_eq from "./src/utils/ifequal";
 
+
+console.log(resolve(__dirname, "build"));
+console.log(resolve(__dirname, 'src/index.html'));
+
 export default defineConfig( {
     root: resolve(__dirname, 'src'),
-
+    build: {
+        outDir: resolve(__dirname, "build"),
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'src/index.html'),
+                auth: resolve(__dirname, 'src/pages/auth/auth.html'),
+                chat1: resolve(__dirname, 'src/pages/chat/chat.html'),
+                chat2: resolve(__dirname, 'src/pages/chat/chat-empty.html'),
+                chat3: resolve(__dirname, 'src/pages/chat/chat-choose-attach.html'),
+                chat4: resolve(__dirname, 'src/pages/chat/chat-choose-attach-pic.html'),
+                chat5: resolve(__dirname, 'src/pages/chat/chat-choose-attach-file.html'),
+                community1: resolve(__dirname, 'src/pages/community/community.html'),
+                community2: resolve(__dirname, 'src/pages/community/community-edit.html'),
+                error404: resolve(__dirname, 'src/pages/error/404.html'),
+                error500: resolve(__dirname, 'src/pages/error/500.html'),
+                password_edit: resolve(__dirname, 'src/pages/password-edit/password-edit.html'),
+                profile1: resolve(__dirname, 'src/pages/profile/profile.html'),
+                profile2: resolve(__dirname, 'src/pages/profile/profile-edit.html'),
+                profile3: resolve(__dirname, 'src/pages/profile/profile-edit-photo.html'),
+                register: resolve(__dirname, 'src/pages/register/register.html')
+            }
+        }
+    },
     plugins: [handlebars({
         helpers: {
             if_eq,
@@ -23,14 +49,7 @@ export default defineConfig( {
             leftside,
             rightside
         },
-        build: {
-            rollupOptions: {
-                input: {
-                    index: resolve(__dirname, 'index.html'),
-                    auth: resolve(__dirname, 'src/pages/auth/auth.html')
-                }
-            }
-        },
+
         partialDirectory: resolve(__dirname, "src/components")      
     })
     ]
