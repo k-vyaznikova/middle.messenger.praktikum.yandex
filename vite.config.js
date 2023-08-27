fa6f44286a2d8254a6c92783d28778de6d8db60f
Vyaznikova Kristina <kristina-vyaz@yandex.ru>
2023-08-27 23:25:23 +0300

    fix для тестов

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import {resolve} from 'path';
-import {defineConfig} from 'vite';
-import handlebars from './vite-plugin-handlebars-precompile';
+import {resolve} from "path";
+import {defineConfig} from "vite";
+import handlebars from "./vite-plugin-handlebars-precompile";
 
 export default defineConfig( {
-    root: resolve(__dirname, 'src'),
-    build: {
-        outDir: resolve(__dirname, "build"),
-    },
-    server: {
-        open: 'index.html'
-    },
-    plugins: [handlebars()]
+	root: resolve(__dirname, "src"),
+	build: {
+		outDir: resolve(__dirname, "build")
+	},
+	server: {
+		open: "index.html"
+	},
+	plugins: [handlebars()]
 
 });
