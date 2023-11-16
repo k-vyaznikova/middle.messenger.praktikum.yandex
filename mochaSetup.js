4c5a2272101d57d852176a2132ea406dd3a90348
Vyaznikova Kristina <kristina-vyaz@yandex.ru>
2023-11-16 16:40:48 +0300

    alias

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,9 +1,11 @@
 const {JSDOM} = require("jsdom");
-//const {resolve} = require("path");
-//require("module-alias/register");
-console.log(process.env.NODE_PATH);
+const moduleAlias = require("module-alias");
+
 const {window} = new JSDOM("<div id='app'></div>", {
     url: "http://localhost:3000"
   });
+    global.window = window;
 
-  global.window = window;
+
+
+    moduleAlias.addAlias("/utils/routing/", __dirname + "/src/utils/routing/");
